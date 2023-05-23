28f10b540f012cf6166c81c491a0817f13c90951
GloryKO <koladeg2022@gmail.com>
2023-05-23 15:25:52 -0700

    added the job tab functionality

--- app/job_details/[id].js
+++ app/job_details/[id].js
@@ -6,9 +6,17 @@ import {Company,JobAbout,JobFooter,JobTabs,ScreenHeaderBtn,Specifics} from '../.
 import {COLORS,icons,SIZES} from '../../constants';
 import useFetch from '../../hook/useFetch';
 
+
+const tabs = ['About','Qualifications','Resposibilities']
 const JobDetails = () => {
     const params = useSearchParams(); //get the id of the job using the search params
     const router = useSearchRouter(); //initialize the router
+    const [refreshing,setRefreshing] = useState(false);
+    const [activeTab,setActiveTab] = useState(tabs[0]);
+
+    const onRefresh =()=>{
+
+    }
     //destructure the ffg from the custom hook useFetch we created 
     const {data,isLoading,error,reFetch} = useFetch('job-details',{
         'job_id': params.id
@@ -22,7 +30,30 @@ const JobDetails = () => {
         headerTitle: " "
         }}
     />
+    <>
+        <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}>
 
+        {isLoading ? (<ActivityIndicator size="large" color={COLORS.primary}/>
+          ): error ? (<Text>Something Went Wrong</Text>) :
+           data.length === 0? (
+            <Text>No Data</Text>
+          ): (
+            <View style={{padding:SIZES.medium,paddingBottom:100}}>
+
+              <Company companyLogo={data[0].employer_logo}
+                       jobTitle={data[0].job_title}
+                       companyName={data[0].employer_name}
+                       location={data[0].job_country}
+              />
+              <JobTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab}/>
+
+            </View>
+          )
+          
+        }
+
+        </ScrollView>
+    </>
     </SafeAreaView>
   )
 }
